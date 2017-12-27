1077d57407d12065df400906adfb4c09efb4af3b
Burtorustum <aburur@d-e.org>
2017-12-27 19:57:32 +0200

    Added some packages to the update list.

--- updatePackages.sh
+++ updatePackages.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo 'updating packages'
 echo ''
+pip3 install pip-upgrader --upgrade;
+pip3 install pip --upgrade;
 pip3 install graphics.py --upgrade;
 pip3 install sympy --upgrade;
 pip3 install numpy --upgrade;
+pip3 install scipy --upgrade;
 pip3 install matplotlib --upgrade;
 echo ''
 echo 'all done'
