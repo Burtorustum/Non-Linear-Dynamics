0e0fc973ec61637b7c4516491c25c0497d6bd240
Burtorustum <aburur@d-e.org>
2017-12-27 20:00:47 +0200

    More updates!

--- updatePackages.sh
+++ updatePackages.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-echo 'updating packages'
+echo 'Updating packages...'
 echo ''
-pip3 install pip-upgrader --upgrade;
-pip3 install pip --upgrade;
-pip3 install graphics.py --upgrade;
-pip3 install sympy --upgrade;
-pip3 install numpy --upgrade;
-pip3 install scipy --upgrade;
-pip3 install matplotlib --upgrade;
+pip3 install pip-upgrader --upgrade
+pip3 install pip --upgrade
+pip3 install graphics.py --upgrade
+pip3 install sympy --upgrade
+pip3 install numpy --upgrade
+pip3 install scipy --upgrade
+pip3 install matplotlib --upgrade
 echo ''
 python3 --version
 pip3 --version
-echo 'all done'
+echo ''
+echo 'All done.'
