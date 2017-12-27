61fb0d78f66fa8ea93b020ce73264c7c82612504
Burtorustum <aburur@d-e.org>
2017-12-27 11:53:14 +0200

    Moved getOption into a seperate utilities folder for useful functions.

---
+++ Utility/getOption.py
@@ -0,0 +1,21 @@
+#getOption.py -- Robert Aburustum
+from math import pi
+def getOption(i,n):
+    x = n+1
+    while x <= i or x >= n:
+        try:
+            x = float(eval(input("Please input a number between " + str(i) + " and " + str(n) + " exclusive. \n")))
+        except ValueError:
+            print("That's not a number.")
+            x = n+1
+        except SyntaxError:
+            print("There is a problem in your input syntax")
+            x = n+1
+    return x
+
+def main():
+        getOption(0,3)
+        print("good job.")
+
+if __name__ == "__main__":
+    main()
