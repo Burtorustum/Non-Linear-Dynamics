d70fd670672fb0f45be3dc410639ae55d9021228
Burtorustum <aburur@d-e.org>
2017-12-29 16:54:29 +0100

    import statements

--- MandelbrotSet/MandelbrotSet.py
+++ MandelbrotSet/MandelbrotSet.py
@@ -1,4 +1,10 @@
 #MandelbrotSet.py
+from graphics import *
+from nld_graphics import *
+import numpy as np
+import time as time
+import math as m
+import cmath as cm
 
 class MandelbrotSet:
     def __init__ ():
