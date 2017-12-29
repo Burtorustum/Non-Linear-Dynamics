a1f7996e659ba876376d2193437f010e1e2f2cfd
Burtorustum <aburur@d-e.org>
2017-12-29 17:33:20 +0100

    Work on implementing with numpy.

--- MandelbrotSet/MandelbrotSet.py
+++ MandelbrotSet/MandelbrotSet.py
@@ -3,9 +3,31 @@ from graphics import *
 from nld_graphics import *
 import numpy as np
 import time as time
-import math as m
-import cmath as cm
+import matplotlib.pyplot as plt
+import matplotlib.colors as color
 
 class MandelbrotSet:
-    def __init__ ():
-        None
+
+    def __init__ (self, pixelWidth, pixelHeight):
+        self.window = NLDGraphWin("Complex Plane", pixelWidth, pixelHeight, [-2,-2,2,2])
+        self.yPoints = np.linspace(-1.4, 1.4, self.window.height)
+        self.xPoints = np.linspace(-2.1, 0.8, self.window.width)
+
+    def plotSet(self, maxIterates=100):
+        y, x = np.ogrid[-1.4:1.4:self.window.height*1j, -2.1:0.8:self.window.width*1j]
+        c = x + y*1j
+        z = c
+        divergeIter = maxIterates + np.zeros(z.shape, dtype=int)
+
+        for i in range(maxIterates):
+            z = z**2 + c
+            diverge = abs(z) > 2
+            divergingNow = diverge & (divergeIter == maxIterates)
+            divergeIter[divergingNow] = i
+            z[diverge] = 2
+
+        return divergeIter
+m = MandelbrotSet(800, 800)
+output = m.plotSet()
+print(m.yPoints)
+print(m.xPoints)
