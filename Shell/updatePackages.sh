d60794d59e620d654ee7d3888c0b5f793fd8397e
Burtorustum <aburur@d-e.org>
2017-12-29 17:43:22 +0100

    Moved shell scripts

---
+++ Shell/updatePackages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo 'Updating packages for NonLinear_Dynamics in Python3...'
+echo ''
+pip3 install pip-upgrader --upgrade
+pip3 install pip --upgrade
+pip3 install graphics.py --upgrade
+pip3 install sympy --upgrade
+pip3 install numpy --upgrade
+pip3 install scipy --upgrade
+pip3 install matplotlib --upgrade
+echo ''
+python3 --version
+pip3 --version
+echo ''
+echo 'All done.'
+exit
