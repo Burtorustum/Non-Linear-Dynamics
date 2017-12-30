3e7cde58bf2ff7ec76f0df1239fdf7a3d54ba862
Burtorustum <aburur@d-e.org>
2017-12-30 16:26:12 +0100

    Got Graphwin part working somewhat

--- MandelbrotSet/MandelbrotSet.py
+++ MandelbrotSet/MandelbrotSet.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 class MandelbrotSet:
 
     def __init__ (self, pixelWidth, pixelHeight):
-        self.window = NLDGraphWin("Complex Plane", pixelWidth, pixelHeight, [-2,-2,2,2])
+        self.window = NLDGraphWin("Complex Plane", pixelWidth, pixelHeight, [-1.4,-2.1,1.4,0.8])
 
     def numPlotSet(self, maxIterates=100):
         y, x = np.ogrid[-1.4:1.4:self.window.height*1j, -2.1:0.8:self.window.width*1j]
@@ -35,7 +35,15 @@ class MandelbrotSet:
             diverge = abs(z) > 2
             z[diverge] = 2
 
-        print(z.shape)
+        for i in range(len(c)):
+            for ii in range(len(c[i])):
+                if diverge[i][ii]:
+                    z = c[i][ii]
+                    self.window.plot(z.real,z.imag)
+        self.window.update()
+
 
 m = MandelbrotSet(800, 800)
 regoutput = m.regPlotSet()
+m.window.getMouse()
+m.window.close()
