28a48913268128d1e7c76bb16a325c25155b2059
Burtorustum <aburur@d-e.org>
2017-12-30 08:18:28 +0100

    Now removes old html file

--- Shell/generateHtml.sh
+++ Shell/generateHtml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+echo ''
 echo 'Generating up to date html file. Will remove old file if it exists.'
 echo ''
-#rm -rf ../MandelbrotSet/MandelbrotSet.html
-
+rm ../MandelbrotSet/MandelbrotSet.html
 jupyter nbconvert --to html ../MandelbrotSet/MandelbrotSet.ipynb
 echo ''
 echo 'Conversion complete!'
