b7b49795a9361d4e4dd1ec8a1f8709357d5c6a5e
Burtorustum <aburur@d-e.org>
2018-01-02 11:05:56 -0500

    Added main file.

---
+++ JuliaSet/main.py
@@ -0,0 +1,13 @@
+#main.py
+from MandelbrotSet import *
+from JuliaSet import *
+
+def main():
+    running = True
+    print("This program is for viewing Julia Sets with varying c values as well as the Mandelbrot Set.")
+
+    while running:
+        None;
+
+if __name__ == '__main__':
+    main()
