8e481fef2a876cc43e347fa4525ae1d15722a7ee
Burtorustum <aburur@d-e.org>
2018-01-02 11:49:07 -0500

    Added todo stuff

--- JuliaSet/main.py
+++ JuliaSet/main.py
@@ -6,6 +6,11 @@ def main():
     running = True
     print("This program is for viewing Julia Sets with varying c values as well as the Mandelbrot Set.")
 
+    #TODO: Choose point on MandelbrotSet and draw corresponding julia Set,
+    #      Color Schemes
+    #      Allow user to change num of iterates
+    #      Optimize iterate amount based on zoom
+    #      
     while running:
         None;
 
