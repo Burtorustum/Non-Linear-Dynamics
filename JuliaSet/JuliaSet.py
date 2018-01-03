cdf4b2e8e3ff73142dc3afed630299ec5a4491de
Burtorustum <aburur@d-e.org>
2018-01-03 16:57:01 -0500

    Added inverse method for drawing extremely quickly

--- JuliaSet/JuliaSet.py
+++ JuliaSet/JuliaSet.py
@@ -3,22 +3,23 @@
 from nld_graphics import *
 import numpy as np
 import time as time
+from random import random
 
 class JuliaSet:
     def __init__ (self, pixelWidth, pixelHeight):
-        self.window = NLDGraphWin("Complex Plane", pixelWidth, pixelHeight, [-2,-2,2,2])
-        self.zoomcount = 0
+        self.hqwindow = NLDGraphWin("HQ Julia Set", pixelWidth, pixelHeight, [-2,-2,2,2])
+        self.lqwindow = NLDGraphWin("LQ Julia Set", pixelWidth, pixelHeight, [-2,-2,2,2])
 
     def regPlotSet(self, maxIterates = 750, const = .365 - 0.37j, fill = False):
         start = time.time()
-        y, x = np.ogrid[self.window.currentCoords[1]:self.window.currentCoords[3]:self.window.height*1j, self.window.currentCoords[0]:self.window.currentCoords[2]:self.window.width*1j]
+        y, x = np.ogrid[self.hqwindow.currentCoords[1]:self.hqwindow.currentCoords[3]:self.hqwindow.height*1j, self.hqwindow.currentCoords[0]:self.hqwindow.currentCoords[2]:self.hqwindow.width*1j]
         c = x + y*1j
         z = c
         divergeIter = maxIterates + np.zeros(z.shape, dtype=int)
 
         for i in range(maxIterates):
             z = z**2 + const
-            diverge = abs(z) >= 3
+            diverge = abs(z) >= 2
             divergingNow = diverge & (divergeIter == maxIterates)
             divergeIter[divergingNow] = i
             z[diverge] = 2
@@ -34,23 +35,46 @@ class JuliaSet:
                         color = 5
                     elif color < 5:
                         color = 0
-                    self.window.plot(z.real, z.imag, color_rgb(color, color, color))
+                    self.hqwindow.plot(z.real, z.imag, color_rgb(color, color, color))
                 elif fill:
                     z = c[i][ii]
-                    self.window.plot(z.real, z.imag, color_rgb(100, 0, 0))
-        self.window.update()
-        print("runtime:", time.time()-start)
+                    self.hqwindow.plot(z.real, z.imag, color_rgb(100, 0, 0))
+        self.hqwindow.update()
+        print("hq runtime:", time.time()-start)
+
+    def inversePlotSet(self, maxIterates = 750, const = .365 - 0.37j):
+        def g(z, c = const):
+            i = 1
+            if random() < .5:
+                i = -1
+            return i * (z-c) ** 0.5
+        start = time.time()
+        z = complex(-1000 + 2000 *random(), -1000 + 2000*random())
+
+        for i in range (10000):
+            z = g(z)
+
+        for i in range(10000):
+            z = g(z)
+            self.lqwindow.plot(z.real, z.imag, 'black')
+
+        self.lqwindow.update()
+        print ("lq runtime: ", time.time()-start)
 
     def zoom(self, inout="in", fill=False):
-        if inout == "in":
-            self.zoomcount += 1
-        else:
-            self.zoomcount = 0
-        self.window.zoom(inout)
+        self.hqwindow.zoom(inout)
         self.regPlotSet(fill=fill)
 
 m = JuliaSet(800, 800)
-m.regPlotSet(fill = False, const= -0.4+0.6j)
+m.regPlotSet(const = 0.35)
+m.hqwindow.getMouse()
+m.hqwindow.close()
+
+#m.inversePlotSet()
+#m.lqwindow.getMouse()
+#m.lqwindow.close()
+
+
 #0
 #1j
 #-1j
@@ -61,5 +85,3 @@ m.regPlotSet(fill = False, const= -0.4+0.6j)
 #-0.835-0.2321j
 #-0.4+0.6j
 #0.279
-m.window.getMouse()
-m.window.close()
