7289e4e3c136ddf37df023f10f6d8aad28a600d3
Burtorustum <burtorustum@gmail.com>
2018-01-29 18:40:49 -0500

    Made more versatile

--- MysteryProgram.py
+++ MysteryProgram.py
@@ -4,54 +4,37 @@ from Utility.getOption import getOption
 import time
 
 window = GraphWin("Mystery Program", 800, 800, False)
-
-print("Select 3 points on the graph.")
-
-#point1 = window.getMouse()
-point1 = Point(200,200)
-window.plot(point1.x, point1.y, 'red')
-window.update()
-
-#point2 = window.getMouse()
-point2 = Point(400,600)
-window.plot(point2.x, point2.y, 'red')
-window.update()
-
-#point3 = window.getMouse()
-point3 = Point(200,600)
-window.plot(point3.x, point3.y, 'red')
-window.update()
-
-#point4 = window.getMouse()
-point4 = Point(400,200)
-window.plot(point4.x, point4.y, 'red')
-window.update()
-
-
-iterations = int(getOption(0, 1000000))
-curPoint = None
-randomPoint = Point(randint(-10000,10000),randint(-10000, 10000))
+endPoints = []
 points = []
-color = None
+lastRand = None
+colors = ['black', 'red', 'blue', 'green', 'purple', 'pink', 'brown', 'teal', 'orange']
+curPoint = None
+
+numPoints = int(input("Input the number of points you want to choose. "))
+print("Choose your points!")
+for x in range(numPoints):
+    endPoints.append(window.getMouse())
+    window.plot(endPoints[-1].x,endPoints[-1].y, 'red')
+    window.update()
+
+print("Choose your number of iterations.")
+iterations = int(getOption(0, 100000))
+randomPoint = Point(randint(-10000, 10000), randint(-10000, 10000))
+
+inverse = True if input("Would you like to plot the inverse fractal? y/n ").lower() == 'y' else False
 
 start = time.time()
 for x in range(iterations):
-    rand = randint(0,3)
-    if rand == 0:
-        curPoint = point1
-        color = 'red'
-    elif rand == 1:
-        curPoint = point2
-        color = 'green'
-    elif rand == 2:
-        curPoint = point3
-        color = 'blue'
-    else:
-        curPoint = point4
-        color = 'black'
-
+    rand = randint(1,numPoints) - 1
+    if inverse:
+        if rand == lastRand:
+            rand += 1
+            if rand == numPoints:
+                rand = 0
+        lastRand = rand
+    curPoint = endPoints[rand]
     randomPoint = Point((randomPoint.x + curPoint.x)/2, (randomPoint.y + curPoint.y)/2)
-    points.append((randomPoint, color))
+    points.append((randomPoint, colors[rand]))
 print(time.time() - start)
 
 start = time.time()
