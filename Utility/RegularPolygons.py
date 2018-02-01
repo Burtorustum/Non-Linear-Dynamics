630e06acb396dea3642a9313a49e57c6718df6d9
Burtorustum <burtorustum@gmail.com>
2018-02-01 09:11:26 -0500

    Moved reg polygons  + made it a useful function that returns the points of a polygon of given num of points

---
+++ Utility/RegularPolygons.py
@@ -0,0 +1,9 @@
+from math import sin, cos, pi
+
+def genPolygonPoints(numPoints, centerpoint = (0,0), radius = 1)
+    vertices = []
+    for n in range(numPoints):
+        x = (radius * cos(2*pi*n/5) + centerpoint[0])
+        y = (radius * sin(2*pi*n/5) + centerpoint[1])
+        vertices.append((x, y))
+    return vertices
