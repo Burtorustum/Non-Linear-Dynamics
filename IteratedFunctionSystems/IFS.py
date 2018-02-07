d218462e6aec59c0712dcf1972034be69abae985
Burtorustum <burtorustum@gmail.com>
2018-02-07 13:43:19 -0500

    Swapped to Radians, added new auto sizing

--- IteratedFunctionSystems/IFS.py
+++ IteratedFunctionSystems/IFS.py
@@ -4,18 +4,27 @@ from graphics import *
 path = os.path.dirname(os.path.abspath(__file__))
 path = path.replace("IFS", "Utility")
 sys.path.insert(0, path)
-from random import randint
+from random import randint, randrange
 import time
 from TransformationObject import *
 
-window = NLDGraphWin("IFS", 800, 800, [0,0,1,1])
+window = NLDGraphWin("IFS", 800, 800, [-1,-1,1,1])
 def main():
     t1 = TransformationObject(r=.5, s=.5)
-    t2 = TransformationObject(r=.5, s=.5, h=.5)
-    t3 = TransformationObject(r=.5, s=.5, h=.25, k=.5)
+    t2 = TransformationObject(r=.5, s=.5, h=.5, theta=45)
+    t3 = TransformationObject(r=.5, s=.5, k=.5)
 
     transformations = [t1, t2, t3]
-    pointeroo = (1,1)
+    pointeroo = (randrange(-1,1),randrange(-1,1))
+
+    random = randint(0,len(transformations)-1)
+    transform = transformations[random]
+    pointeroo = transform.apply(pointeroo)
+
+    minX = pointeroo[0]
+    minY = pointeroo[1]
+    maxX = pointeroo[0]
+    maxY = pointeroo[1]
 
     print("Transients")
     for x in range(1000000):
@@ -23,14 +32,30 @@ def main():
         transform = transformations[random]
         pointeroo = transform.apply(pointeroo)
 
+        if pointeroo[0] > maxX:
+            maxX  = pointeroo[0]
+        elif pointeroo[0] < minX:
+            minX = pointeroo[0]
+
+        if pointeroo[1] > maxY:
+            maxY = pointeroo[1]
+        elif pointeroo[1] < minY:
+            minY = pointeroo[1]
+
+
+    if maxX - minX > maxY - minY:
+        window.setCoords(minX - .1, minX - .1, maxX + .1, maxX + .1)
+    else:
+        window.setCoords(minY - .1, minY - .1, maxY + .1, maxY + .1)
+
     print("Plotting")
     startime = time.time()
-    for x in range(100000):
+    for x in range(250000):
         random = randint(0,len(transformations)-1)
         transform = transformations[random]
         pointeroo = transform.apply(pointeroo)
         window.plot(pointeroo[0], pointeroo[1])
-        if x % 5000 == 0:
+        if x % 10000 == 0:
             window.update()
     print(time.time()-startime)
 
--- IteratedFunctionSystems/TransformationObject.py
+++ IteratedFunctionSystems/TransformationObject.py
@@ -1,24 +1,18 @@
 #Transformation.py
 from graphics import Point
-from math import sin, cos
+from math import sin, cos, pi
 
 class TransformationObject:
     def __init__(self, r=1, s=1, theta=0, phi=None, h=0, k=0):
         self.phi = theta if phi == None else phi
         self.r = r
         self.s = s
-        self.theta = theta
+        self.theta = theta * pi/180
+        self.phi = self.phi * pi/180
         self.h = h
         self.k = k
-
-    def transform(self, points):
-        transformedPoints = []
-        for point in points:
-            transformedPoints.append(self.apply(point))
-        return transformedPoints
 
     def apply(self, point):
         x = self.r * cos(self.theta) * point[0] - self.s * sin(self.phi) * point[1] + self.h
         y = self.r * sin(self.theta) * point[0] + self.s * cos(self.phi) * point[1] + self.k
-
         return (x, y)
