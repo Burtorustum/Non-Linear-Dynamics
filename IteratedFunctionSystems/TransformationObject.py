8e92687fbb3c4108d79b6d0f5d988b504832081c
Burtorustum <burtorustum@gmail.com>
2018-02-15 11:48:38 -0500

    write the string input function

--- IteratedFunctionSystems/TransformationObject.py
+++ IteratedFunctionSystems/TransformationObject.py
@@ -13,6 +13,18 @@ class TransformationObject:
         self.phi = self.phiDeg * pi/180
         self.h = h
         self.k = k
+        self.partList = ['r: ', 's: ', 'theta: ', 'phi: ', 'h: ', 'k: ']
+
+    def changeTransform(self, r=1, s=1, theta=0, phi=None, h=0, k=0, probability=None):
+        self.probability = r if probability == None else probability
+        self.phiDeg = theta if phi == None else phi
+        self.thetaDeg = theta
+        self.r = r
+        self.s = s
+        self.theta = theta * pi/180
+        self.phi = self.phiDeg * pi/180
+        self.h = h
+        self.k = k
 
     def apply(self, point):
         x = self.r * cos(self.theta) * point[0] - self.s * sin(self.phi) * point[1] + self.h
@@ -20,7 +32,18 @@ class TransformationObject:
         return (x, y)
 
     def strToVals(self, string):
-        return None
+        strArr = string.split("|")
+        strArr.pop(-1)
+        strArr.pop(0)
+        newTransforms = []
+
+        for part in strArr:
+            #print(part)
+            nexty = part.split(": ")[1]
+            newTransforms.append(nexty)
+
+        newTransforms = [float(newTransforms[0]), float(newTransforms[1]), float(newTransforms[2]), float(newTransforms[3]), float(newTransforms[4]), float(newTransforms[5])]
+        self.changeTransform(newTransforms[0], newTransforms[1], newTransforms[2], newTransforms[3], newTransforms[4], newTransforms[5])
 
     def __str__(self):
-        return "| r: " + str(self.r) + " | s: " + str(self.s) + " | theta: " + str(self.thetaDeg) + " | phi: " + str(self.phiDeg) + " | h: " + str(self.h) + " | k: " + str(self.k) + " | probabiliy: " + str(self.probability) + " |"
+        return "|r: " + str(self.r) + "|s: " + str(self.s) + "|theta: " + str(self.thetaDeg) + "|phi: " + str(self.phiDeg) + "|h: " + str(self.h) + "|k: " + str(self.k) + "|Probability: " + str(self.probability) + "|"
