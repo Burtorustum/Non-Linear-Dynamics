0c8867666f7be7dbb0f96446157dd5ed1213d9b3
Burtorustum <burtorustum@gmail.com>
2018-05-09 22:06:17 -0400

    pretttyyy

--- FractalTree/FractalTree.py
+++ FractalTree/FractalTree.py
@@ -2,33 +2,42 @@
 import copy as copy
 import math as math
 from nld_graphics import *
-from graphics import *
+
 class FractalTree:
 
     def __init__(self, initialAngle, scaleFactor):
-        self.window = NLDGraphWin("Fractal Tree", 600, 600, [-5,-.1,5,4.1])
+        self.window = NLDGraphWin("Fractal Tree", 600, 600, [-5,-.1,5,9.1])
         self.initialAngle = math.radians(initialAngle)
         self.scaleFactor = scaleFactor
 
-    def drawLine(self, startPoint, length, direction):
+    def drawLine(self, startPoint, length, direction, color='black'):
         ogPoint = copy.copy(startPoint)
         deltaX = length * math.cos(direction)
         deltaY = length * math.sin(direction)
         startPoint.move(deltaX, deltaY)
-        Line(ogPoint, startPoint).draw(self.window)
+        l = Line(ogPoint, startPoint)
+        l.setFill(color)
+        l.setWidth(3)
+        l.draw(self.window)
 
-    def drawTreeRec(self, level, point, length, angle, animated=True):
+    def drawTreeRec(self, level, point, length, angle, animated=False, colored=True, randomLength=False, leftFactor=1, rightFactor=1):
+        if level < 2:
+            self.drawLine(point, length, angle, 'darkgreen')
+        else:
+            self.drawLine(point, length, angle, 'saddlebrown')
         if level != -1:
-            self.drawLine(point, length, angle)
+            #Could change where branches off from AND how many branches come from another branch (for loop system for recursive calls)
             point2 = copy.copy(point)
-            self.drawTreeRec(level-1, point, length * self.scaleFactor, angle - self.initialAngle)
-            self.drawTreeRec(level-1, point2, length * self.scaleFactor, angle + self.initialAngle)
+            self.drawTreeRec(level-1, point, length * self.scaleFactor, angle - rightFactor*self.initialAngle)
+            self.drawTreeRec(level-1, point2, length * self.scaleFactor, angle + leftFactor*self.initialAngle)
             if animated:
                 self.window.update()
 
 def main():
     tree = FractalTree(30, .75)
-    tree.drawTreeRec(9, Point(0,0), 1, math.radians(90))
+
+    tree.drawTreeRec(9, Point(0,0), 1, math.radians(90), rightFactor=.25)
+
     tree.window.getMouse()
     tree.window.close()
 
