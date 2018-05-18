57445687bfb95ed04d49ffd67e363ed210dac29c
Burtorustum <burtorustum@gmail.com>
2018-05-18 10:33:34 -0400

    quit functionality

--- FractalTree/FractalTreeMain.py
+++ FractalTree/FractalTreeMain.py
@@ -1,15 +1,18 @@
 #FractalTreeMain.py
 from FractalTree import FractalTree
+from nld_graphics import *
 from graphics import *
 import math as math
+from getOption import getOption
 
 def main():
-    tree = FractalTree(90, .75)
+    window = NLDGraphWin("Fractal Tree", 600, 600, [-5,-5,5,5])
+    defaultTree = FractalTree(30, .75, window=window)
+    defaultTree.drawTreeRec(6, Point(0,0), 1, math.radians(90))
 
-    tree.drawTreeRec(13, Point(0,0), 1, math.radians(90), colored=True, animated=False, leftFactor=1, rightFactor=1, assymetricLevel=0, widthScaling=False, width=3)
-
-    tree.window.getMouse()
-    tree.window.close()
+    inp = ''
+    while inp != 'quit' and inp != 'q':
+        inp = str.lower(input(' ~ '))
 
 if __name__ == '__main__':
     main()
